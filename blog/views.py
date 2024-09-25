b9c287813aaf6d35e8f6f712ce14403acb04347e
royMatar <roy.mattar@outlook.com>
2024-09-25 23:00:41 +0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from blog.models import Post
+from django.shortcuts import redirect
+from blog.forms import CommentForm
+import logging
+# Create your views here.
+
+logger = logging.getLogger(__name__)
+
+
+def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
+    logger.debug("Got %d posts", len(posts))
+    return render(request, "index.html", {"posts": posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.user.is_active:
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
+
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.content_object = post
+                comment.creator = request.user
+                comment.save()
+                return redirect(request.path_info)
+        else:
+            comment_form = CommentForm()
+    else:
+        comment_form = None
+    return render(request, "post-detail.html", {"post": post,"comment_form": comment_form })
